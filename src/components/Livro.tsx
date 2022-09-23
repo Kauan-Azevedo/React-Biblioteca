0ca2e89563f827a5609f606cebe2e9b747244103
Kauan-Azevedo <kauanazevedo2010@hotmail.com>
2022-09-23 16:21:58 -0300

    feat: adicionando livro

--- src/components/Livro.tsx
+++ src/components/Livro.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import api from "../services/api";
+import LivroDTO from "../types/livro";
 
 export default function Livro() {
-  // const getLivro = async () => {
-  //     await api.
-  // }
+  let url = window.location.href.slice(28);
+  const [livro, setLivro] = useState<LivroDTO>();
 
+  const getLivro = async () => {
+    const { data } = await api.get(`/books/${url}`);
+    const prod = data;
+    setLivro(prod);
+    console.log(prod);
+  };
+
+  useEffect(() => {
+    getLivro();
+  }, [window.location.href]);
   return (
     <>
-      <h2>Funcionou!</h2>
+      {livro && (
+        <div className="livro">
+          <h2 className="livro__title">{livro?.title}</h2>
+          <p className="livro__body">{livro?.description}</p>
+          <p>Quantidade de paginas: {livro?.qnt_paginas}</p>
+          <p>Quantidade disponiveis: {livro?.amount}</p>
+          <p>Codigo de barras: {livro?.bar_code}</p>
+        </div>
+      )}
     </>
   );
 }
---
+++ src/types/livro.ts
@@ -0,0 +1,12 @@
+
+interface LivroDTO {
+    id?: string,
+    image_url?: string,
+    title?: string,
+    description?: string,
+    amount?: number,
+    bar_code?: string,
+    qnt_paginas?: number
+}
+
+export default LivroDTO;
